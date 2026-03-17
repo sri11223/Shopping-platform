f1275a5292de4bb5435527233aa65fc77de84052
sri11223 <srikrishnanutalapati@gmail.com>
2026-03-17 22:16:01 +0530

    feat: added render deployment link in frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  razorpayKeyId: 'rzp_test_placeholder',
+  apiUrl: 'https://shopping-platform-5quz.onrender.com/api',
+  razorpayKeyId: 'rzp_test_SSHZdur0rnT5NY', // Using your test key
 };
